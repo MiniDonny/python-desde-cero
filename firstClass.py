1826bac5778b35531876f1bed86659d3025f8a5b
Donato Robledo <129457178+MiniDonny@users.noreply.github.com>
2023-11-04 12:14:57 -0300

    Add files via upload

---
+++ firstClass.py
@@ -0,0 +1,11 @@
+# My first "Hellow world" in Python
+print("Hello world")
+# Print of date's today = dd/mm/aaaa
+day = 4
+month = 11
+year = 2023
+print("Today is:",day,"/",month,"/",year)
+# Print of a person
+name = "Donato"
+age = 18
+print(f"Mi nombre es: {name} y tengo {age} años")
